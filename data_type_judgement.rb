b1b2414566bb573de06250d61031db264747235d
{Kaito} <{sirobu9963@gmail.com}>
2023-07-22 23:02:51 +0900

    未定義データの判定を追加

--- data_type_judgement.rb
+++ data_type_judgement.rb
@@ -5,8 +5,10 @@ def data_type_judgement(data)
     puts('Numeric です。')
   elsif data.is_a?(Array)
     puts('Array です。') 
-  else data.is_a?(Hash)
+  elsif data.is_a?(Hash)
     puts('Hash です。')
+  else
+    puts('判定できないデータです。')
   end
 end
 
@@ -14,3 +16,9 @@ puts(data_type_judgement('123'))
 puts(data_type_judgement(123))
 puts(data_type_judgement([1, 2, 3]))
 puts(data_type_judgement({"数字１" => 1, "数字2" => 2, "数字3" => 3}))
+
+class User
+end
+
+user = User.new
+puts(data_type_judgement(user))
