006a198ad5b63f23e6e5bd3a03931671a1b1a3bb
Kaito1290 <ono.kaito569@mail.kyutech.jp>
2023-08-08 22:32:44 +0900

    .sqlファイルを作成

---
+++ docker/mysql/.sql
@@ -0,0 +1,8 @@
+create table mydb.user(id int, name varchar(10));
+
+insert into mydb.user values (1, 'Kaito');
+insert into mydb.user values (2, 'Shoichi');
+insert into mydb.user values (3, 'Daisuke');
+insert into mydb.user values (4, 'Ryosuke');
+insert into mydb.user values (5, 'Hiiro');
+
