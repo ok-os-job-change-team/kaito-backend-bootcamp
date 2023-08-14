7e26702fe4e264e3e987b701a88b4fcf88d5e6d6
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-08-14 10:39:10 +0900

    キーワード引数とデフォルト値を設定

--- models/test/user_test.rb
+++ models/test/user_test.rb
@@ -5,13 +5,13 @@ class UserTest < Minitest::Test
 
   # ユーザー1 => インスタンス変数がすべて揃っている場合
   def test_user_1
-    user_1 = User.new(1, 'kaito@exampe.co.jp', 'p@ssw0rd_1', '2023-08-11', '2023-08-12')
+    user_1 = User.new(id: 1, email: 'kaito@exampe.co.jp', password: 'p@ssw0rd_1', created_at: '2023-08-11', updated_at: '2023-08-12')
     assert user_1.present?
   end
 
   # ユーザー2 => インスタンス変数が不足している場合
   def test_user_2
-    user_2 = User.new(nil, 'shoichi@exampe.co.jp', 'p@ssw0rd_2', '2023-08-11', '2023-08-12')
+    user_2 = User.new(id: nil, email: 'shoichi@exampe.co.jp', password: 'p@ssw0rd_2', created_at: '2023-08-11', updated_at:'2023-08-12')
     refute user_2.present?
   end
 end
--- models/user.rb
+++ models/user.rb
@@ -1,7 +1,7 @@
 class User
   attr_reader :id, :email, :password, :created_at, :updated_at
 
-  def initialize(id, email, password, created_at, updated_at)
+  def initialize(id:, email:, password:, created_at: Time.now, updated_at: Time.now)
     @id = id
     @email = email
     @password = password
