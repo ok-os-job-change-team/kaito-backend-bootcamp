82ba955c96c445541ef1cd5d29c0bf901a18bd0a
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-08-21 00:19:13 +0900

    外部キー制約を追加

--- sql/favorites.sql
+++ sql/favorites.sql
@@ -2,4 +2,8 @@ CREATE TABLE Favorites(
   id INT PRIMARY KEY,
   user_id INT,
   tweet_id INT,
+  FOREIGN KEY (user_id)
+  REFERENCES Users(id)
+  FOREIGN KEY (tweet_id)
+  REFERENCES Tweets(id)
 );
