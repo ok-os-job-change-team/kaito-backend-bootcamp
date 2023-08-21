7edd3ae5e880fb59ddcbb9b178d822481e208b3b
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-08-21 23:30:17 +0900

    Favoriteクラスの修正、Favoriteクラスに対してrspecを作成

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,11 +1,11 @@
 class Favorite
   attr_reader :id, :user_id, :tweet_id, :errors
 
-  def initialize(id:, user_id:, tweet_id:, errors:)
+  def initialize(id:, user_id:, tweet_id:)
     @id = id
     @user_id = user_id
     @tweet_id = tweet_id
-    @errors = errors
+    @errors = Array.new
   end
 
   def present?
