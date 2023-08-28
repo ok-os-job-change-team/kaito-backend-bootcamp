c02713d7dc9e0d7555917cda19ca984b72834692
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-08-28 23:54:51 +0900

    Usersテーブルにレコードを挿入するsqlファイルを作成

---
+++ sql/users_tabale_insertion.sql
@@ -0,0 +1,12 @@
+INSERT INTO Users (id, email, pass, created_at, updated_at)
+VALUES
+(1, 'user1@example.com', 'pass12345', '2023-01-01 10:00:00', '2023-01-01 10:00:00'),
+(2, 'user2@example.com', 'pass67890', '2023-01-02 11:00:00', '2023-01-02 11:00:00'),
+(3, 'user3@example.com', 'passABCDE', '2023-01-03 12:00:00', '2023-01-03 12:00:00'),
+(4, 'user4@example.com', 'pass54321', '2023-01-04 13:00:00', '2023-01-04 13:00:00'),
+(5, 'user5@example.com', 'pass09876', '2023-01-05 14:00:00', '2023-01-05 14:00:00'),
+(6, 'user6@example.com', 'passXYZAB', '2023-01-06 15:00:00', '2023-01-06 15:00:00'),
+(7, 'user7@example.com', 'passHELLO', '2023-01-07 16:00:00', '2023-01-07 16:00:00'),
+(8, 'user8@example.com', 'passWORLD', '2023-01-08 17:00:00', '2023-01-08 17:00:00'),
+(9, 'user9@example.com', 'passTURING', '2023-01-09 18:00:00', '2023-01-09 18:00:00'),
+(10, 'user10@example.com', 'passCOMPLETE', '2023-01-10 19:00:00', '2023-01-10 19:00:00');
