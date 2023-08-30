9f774935cbbf82e3c404d4ee6e72056946d5cbbc
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-08-30 20:04:19 +0900

    Tweetsテーブルにレコードを挿入するsqlファイルを作成

--- sql/tweets.sql
+++ sql/tweets.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Tweets(
   id INT PRIMARY KEY,
   user_id INT,
-  title varchar(30),
-  tweet_content varchar(140),
+  title varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  tweet_content varchar(140) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   FOREIGN KEY (user_id)
   REFERENCES Users(id)
 );
---
+++ sql/tweets_table_insertion.sql
@@ -0,0 +1,13 @@
+INSERT INTO Tweets (id, user_id, title, tweet_content)
+VALUES
+(1, 1, 'Hello World', 'This is my first tweet! #hello'),
+(2, 2, 'Learning SQL', 'Databases are fascinating! #sql'),
+(3, 3, 'At the beach', 'Enjoying the sunny day! #beach'),
+(4, 4, 'Reading a book', 'Just started a new fantasy novel. #reading'),
+(5, 5, 'Workout time', 'Time to hit the gym. #fitness'),
+(6, 6, 'Travel Diaries', 'Exploring the mountains today! #travel'),
+(7, 7, 'Foodie alert', 'Tried sushi for the first time. #food'),
+(8, 1, 'Music vibes', 'Listening to some lo-fi. #music'),
+(9, 3, 'Gaming night', 'Playing my favorite video game. #gaming'),
+(10, 2, 'Starry night', 'Stargazing tonight. The sky looks beautiful! #stars');
+(11, 1, 'こんにちは', '会社行きたくねえええ');
